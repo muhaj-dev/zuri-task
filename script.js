07eef5b3cc7da854cf7afdc701bc0f33e2db27ea
muhammod1 <muhammodholawale@gmail.com>
2022-08-10 07:32:25 +0100

    modal

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let modal = document.getElementById("myModal");
+
+let btn = document.getElementById("myBtn");
+
 const parentContainer = document.querySelector(".read-more-container");
 
 parentContainer.addEventListener("click", event =>{
@@ -12,26 +16,11 @@ parentContainer.addEventListener("click", event =>{
 
 })
 
-// Get the modal
-let modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
